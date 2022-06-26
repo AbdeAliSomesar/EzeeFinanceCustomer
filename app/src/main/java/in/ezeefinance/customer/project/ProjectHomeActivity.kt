420277757e867e647e458eb865bb9e710514b404
Abde Ali Somesar <abdeali.somesar@protonmail.com>
2022-06-27 00:55:56 +0530

    added bottom nav

--- app/src/main/java/in/ezeefinance/customer/project/ProjectHomeActivity.kt
+++ app/src/main/java/in/ezeefinance/customer/project/ProjectHomeActivity.kt
@@ -1,12 +1,24 @@
 package `in`.ezeefinance.customer.project
 
 import `in`.ezeefinance.customer.R
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.appcompat.app.AppCompatActivity
 
 class ProjectHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_home)
+    }
+
+    companion object : ActivityResultContract<Boolean?, Boolean>() {
+        override fun createIntent(context: Context, input: Boolean?) = Intent().apply {
+            setClass(context, ProjectHomeActivity::class.java)
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?) = resultCode == RESULT_OK
+
     }
 }

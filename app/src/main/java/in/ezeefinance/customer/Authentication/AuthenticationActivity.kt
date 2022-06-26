749130fc61ec328f5520561a68b5e0718503b46b
Abde Ali Somesar <abdeali.somesar@protonmail.com>
2022-06-26 15:07:35 +0530

    Initial commit

---
+++ app/src/main/java/in/ezeefinance/customer/Authentication/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package `in`.ezeefinance.customer.Authentication
+
+import `in`.ezeefinance.customer.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication_actiivty)
+    }
+}
